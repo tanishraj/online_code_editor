d841b7b0d15455f1eb9bc77709b4ab625e17c5d1
tanishraj <learn.tanishraj@gmail.com>
2024-05-31 00:06:55 +0530

    feat: language selector drop down component

--- src/components/LanguageSelector.jsx
+++ src/components/LanguageSelector.jsx
@@ -10,24 +10,30 @@ import {
 import { LANGUAGE_VERSIONS } from "../utils/constants";
 
 const languages = Object.entries(LANGUAGE_VERSIONS);
-const ACTIVE_COLOR = "blue.400";
+const ACTIVE_COLOR = "gray.900";
 
 export const LanguageSelector = ({ language, onSelect }) => {
   return (
     <Menu>
-      <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
+      <MenuButton
+        as={Button}
+        rightIcon={<ChevronDownIcon />}
+        variant="outline"
+        colorScheme="green"
+        mb={4}
+      >
         {language}
       </MenuButton>
-      <MenuList>
+      <MenuList colorScheme="green">
         {languages.map(([lang, version]) => {
           return (
             <MenuItem
               key={lang}
               color={lang === language ? ACTIVE_COLOR : ""}
-              bg={lang === language ? "gray.900" : "transparent"}
+              bg={lang === language ? "gray.100" : "transparent"}
               _hover={{
                 color: ACTIVE_COLOR,
-                bg: "gray.900"
+                bg: "gray.100"
               }}
               onClick={() => {
                 onSelect(lang);
@@ -35,7 +41,12 @@ export const LanguageSelector = ({ language, onSelect }) => {
             >
               {lang}
               &nbsp;
-              <Text as="span" color="gray.600" fontSize="sm">
+              <Text
+                as="span"
+                color="gray.900"
+                _dark={{ color: "gray.400" }}
+                fontSize="sm"
+              >
                 ({version})
               </Text>
             </MenuItem>
